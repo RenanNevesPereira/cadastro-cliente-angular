f65cbb539654d7dc6651f2492e559cfb6329d528
RenanNevesPereira <renan.nevespereira@yahoo.com>
2025-06-21 21:17:08 -0300

    index bug fixt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'cadastro' , component: CadastroComponent },
+    { path: 'cliente' , component: ClienteComponent }
+];
