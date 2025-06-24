8fdc23b4973bab7cba8d675d6ba48207a9c8911d
RenanNevesPereira <renan.nevespereira@yahoo.com>
2025-06-24 18:33:35 -0300

    adiciona suporte a máscara de entrada e funcionalidades de atualização e exclusão de clientes

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -6,8 +6,11 @@ import { MatInputModule } from '@angular/material/input'
 import { FormsModule } from '@angular/forms'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import { MatSnackBar } from '@angular/material/snack-bar'
 import { Cliente } from './cliente'
 import { ClienteService } from '../cliente.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-cadastro',
@@ -18,25 +21,55 @@ import { ClienteService } from '../cliente.service';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    NgxMaskDirective,
+  ], providers: [
+    provideNgxMask()
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
 
   cliente: Cliente = Cliente.newCliente();
+  atualizando: boolean = false;
+  snack: MatSnackBar = inject(MatSnackBar)
 
-  constructor(private service: ClienteService){
+  constructor(
+    private service: ClienteService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){
 
+  }
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe( (query: any) => {
+        const params = query['params']
+        const id = params['id']
+        if(id){
+          let clienteEncontrado = this.service.buscarClientePorId(id);
+          if(clienteEncontrado){
+            this.atualizando = true;
+            this.cliente = clienteEncontrado;
+          }
+        }
+    })
   }
 
   salvar(){
-    this.service.salvar(this.cliente);
-    console.log('Cliente salvo:', this.cliente);
+    if(!this.atualizando){
+      this.service.salvar(this.cliente);
+      this.cliente = Cliente.newCliente();
+      this.mostrarMensagem('Salvo com sucesso!')
+    } else {
+      this.service.atualizar(this.cliente);
+      this.router.navigate(['/consulta'])
+      this.mostrarMensagem('Atualizado com sucesso!')
+    }
   }
 
-  limparFormulario(form: any) {
-    form.resetForm(); // limpa o form e o model associado
+  mostrarMensagem(mensagem: string){
+    this.snack.open(mensagem, "Ok")
   }
 }
